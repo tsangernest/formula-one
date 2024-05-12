7e64d1f9ede7f6d1d63e5f8682f2d7405bb1c4ef
p14s <tsangernest@hotmail.com>
2024-05-11 21:52:55 -0500

    * logs and exec shortcuts

--- tasks.py
+++ tasks.py
@@ -1,15 +1,17 @@
+from typing import NoReturn
+
 from invoke import task
 from settings import APP_NAME
 
 
 @task
-def build(c) -> None:
+def build(c) -> NoReturn:
     cmd: str = f"docker build . -t {APP_NAME}"
     c.run(cmd, pty=True)
 
 
 @task
-def run(c, reset: bool = False):
+def run(c, reset: bool = False) -> NoReturn:
     if reset:
         build(c)
     cmd: str = f"docker run --name {APP_NAME} -it -p 8000:8000 {APP_NAME}"
@@ -17,13 +19,13 @@ def run(c, reset: bool = False):
 
 
 @task
-def logs(c, container_name: str = f"{APP_NAME}"):
+def logs(c, container_name: str = f"{APP_NAME}") -> NoReturn:
     cmd: str = f"docker logs -f --details {container_name}"
     c.run(cmd, pty=True)
 
 
 @task
-def exec(c):
+def exec(c) -> NoReturn:
     cmd: str = f"docker exec -it {APP_NAME} /bin/bash"
     c.run(cmd, pty=True)
 
